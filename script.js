f0d6c0ef001cf79088d1ed76f7df413ceffe4edc
dabkenar <dabkenar@umich.edu>
2020-04-02 21:25:24 -0400

    good to submit, might try and fix tab issues

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-Vue.prototype.$appName = 'Music Search'
-
-new Vue({
-  beforeCreate: function() {
-    console.log(this.$appName)
-  }
-})
-
 var resultView = new Vue({
   el: '#app',
   data: {
-    artist1: './img/1.jpg',
     numResults: 0,
     queryInput: '',
     queryWords: [],
@@ -18,6 +9,7 @@ var resultView = new Vue({
     requestDump: [],
     filters:[],
     tab1: true,
+    tab2: false,
     filteredResults: false,
   },
   methods: {
@@ -26,9 +18,17 @@ var resultView = new Vue({
       .get('https://itunes.apple.com/search?term=' + this.queryInput + '&origin=*')
       .then(response => {
         console.log(response.data); //Print API response to console
+
+        if (response.data.resultCount === 0) { //If No results are found print this message
+          alert("No Artists Found");
+          this.queryInput = '';
+          return;
+        }
+
         this.numResults = response.data.resultCount; //Set number of returned items
+
         this.results = []; //Reset results array
-        this.filters = [];
+        this.filters = [{genre: 'All', active: true}];
         this.filteredResults = false;
         this.requestDump = [];
 
@@ -41,9 +41,6 @@ var resultView = new Vue({
         
         this.results = this.requestDump.slice();
         
-        if (this.numResults === 0) { //If No results are found print this message
-          alert("No Artists Found");
-        }
       });
     },
     parse_query () {
@@ -66,6 +63,7 @@ var resultView = new Vue({
     },
     add_filter (filter) {
       console.log("Filter Added");
+      this.filters[0].active = false;
       if (!this.filteredResults) {
         this.results = [];
         this.filteredResults = true;
@@ -88,6 +86,7 @@ var resultView = new Vue({
       this.results = [];
       this.results = newResults.slice();
       if (this.results.length === 0) {
+        this.filters[0].active = true;
         this.results = this.requestDump.slice();
         this.filteredResults = false;
       }
@@ -99,6 +98,17 @@ var resultView = new Vue({
         }
       }
       return true;
+    },
+    remove_all_filters () {
+      if (this.filters[0].active) {
+        return;
+      }
+      for (let i in this.filters) {
+          this.filters[i].active = false;
+      }
+      this.filters[0].active = true;
+      this.results = this.requestDump.slice();
+      this.filteredResults = false;
     }
   }
 })
@@ -106,7 +116,9 @@ var resultView = new Vue({
 Vue.component('genre-filter', {
   props: ['filter'],
   template:
-  `<button v-if="!filter.active" class="btn btn-primary" style="margin:2px;" v-on:click="toggle_filter(); resultView.add_filter(filter.genre)" >{{filter.genre}}</button>
+  `
+  <button v-if="filter.genre == 'All'" class="btn btn-success" style="margin:2px;" v-on:click="resultView.remove_all_filters()" >{{filter.genre}}</button> 
+  <button v-else-if="!filter.active" class="btn btn-primary" style="margin:2px;" v-on:click="toggle_filter(); resultView.add_filter(filter.genre)" >{{filter.genre}}</button>
    <button v-else class="btn btn-info" style="margin:2px;" v-on:click="toggle_filter(); resultView.remove_filter(filter.genre)" >{{filter.genre}}</button>`,
   methods: {
     toggle_filter () {
@@ -116,7 +128,7 @@ Vue.component('genre-filter', {
 }) 
 
 Vue.component('search-result', {
-  props: ['result', 'tab1'],
+  props: ['result', 'tab1', 'tab2'],
   template:
 `    <div id="search-result" class = "col-lg-6 col-md-6 col-sm-6" style='margin-bottom: 50px; display: inline; height: 250px' >
       <div class="col-sm-4" style='max-width:200px;margin-right: 30px'>
@@ -126,8 +138,8 @@ Vue.component('search-result', {
       <div>
         <div id="toggle-tabs" class="col-sm-6" style='max-height: 250px;'>
             <ul class="nav nav-tabs">
-              <li href="#tab1-content" class="active"><a data-toggle="tab" v-on:click="switch_tab">Description</a></li>
-              <li><a href="#tab2-content" data-toggle="tab" v-on:click="switch_tab">Artist Info.</a></li>
+              <li href="#tab1-content" class="active"><a data-toggle="tab" v-on:click="click_tab1">Description</a></li>
+              <li><a href="#tab2-content" data-toggle="tab" v-on:click="click_tab2">Artist Info.</a></li>
             </ul>
             <div v-if="tab1" id="tab1-content">
               <span>
@@ -184,8 +196,13 @@ Vue.component('search-result', {
       }
       return returnVal;
     },
-    switch_tab () {
-      this.tab1 = !this.tab1;
+    click_tab1 () {
+      this.tab1 = true;
+      this.tab2 = false;
+    },
+    click_tab2 () {
+      this.tab1 = false;
+      this.tab2 = true;
     }
   }
 })
